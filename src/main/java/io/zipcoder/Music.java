f8100d454cdfe989f4239fa409304045cbb0161f
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-11-26 21:12:18 -0500

    passed tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,21 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int toReach = 0;
+        int forward = 0;
+        int backward = 0;
+        for (int i = 0; i < playList.length; i++) {
+            if (selection.equals(playList[i])) {
+                forward = i - startIndex;
+                backward = startIndex - i + playList.length;
+                if (forward > backward) {
+                    toReach = backward;
+                } else {
+                    toReach = forward;
+                }
+            }
+        }
+        return toReach;
+//        return null;
     }
 }
